b2459cf8cffe35e9d1525adddc3bdd7c5ae88fd6
Leo Ngari <ngarimwenda@gmail.com>
2023-12-20 10:07:49 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,7 @@
+function calculateNumbers(value1, value2) {
+    const sum = value1 + value2
+    const sub = value1 - value2
+    const mul = value1 * value2
+    const div = value1 / value2
+    return [sum, sub, mul, div]
+}
